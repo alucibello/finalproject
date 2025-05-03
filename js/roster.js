65c99131a8fbe87a84b1d894215709bd946690d5
alucibello <alucibello@mail.endicott.edu>
2025-05-02 23:22:20 -0400

    Final

---
+++ js/roster.js
@@ -0,0 +1,21 @@
+//get current year for footer
+document.getElementById("copyright").innerHTML = new Date().getFullYear();
+
+
+//return to top button
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+         
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("cornerbtn").style.display = "block";
+  } else {
+    document.getElementById("cornerbtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
